15cefea4d31120c299fa9d681feef12f5c14ace0
William "Gus" Wedertz <gusweeds@outlook.com>
2025-02-13 14:08:28 -0500

    Add files via upload

---
+++ alarmClock.py
@@ -0,0 +1,32 @@
+# Python Alarm Clock
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+    print (f"Alarm set for {alarm_time}")
+    sound_file = "alarm_sound.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%I:%M:%S %p")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("Time to Wake Up!")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            
+            is_running = False
+
+        time.sleep(1)
+       
+
+if __name__ == "__main__":
+    alarm_time = input("Enter alarm time (HH:MM:SS AM/PM): ")
+    set_alarm(alarm_time)
